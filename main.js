d3cb95712502b2c0cbe98cd84a4047d6beaf9ce7
tomeytran <tmtri310251@gmail.com>
2024-09-12 23:13:40 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+//  Bai 1
+
+let browser2 = prompt("Enter browser name!");
+switch (browser2) {
+	case "Edge":
+		alert("You've got the Edge!");
+		break;
+	case "Chrome":
+	case "Firefox":
+	case "Safari":
+	case "Opera":
+		alert("Okay we support these browsers too");
+		break;
+	default:
+		alert("We hope that this page looks ok!");
+}
+
+// Bai 2
+let a = +prompt("a?", "");
+switch (a) {
+	case 0:
+		alert(0);
+		break;
+	case 1:
+		alert(1);
+		break;
+	case 2:
+	case 3:
+		alert("2,3");
+		break;
+}
+
+// Bai 3
