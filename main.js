7adea6c104e674bbca817346c57a4772902064c8
tomeytran <tmtri310251@gmail.com>
2024-09-13 08:44:16 +0700

    update bai 3

--- main.js
+++ main.js
@@ -31,3 +31,22 @@ switch (a) {
 }
 
 // Bai 3
+let value = +prompt("Type a number", 0);
+switch (value) {
+	case 5:
+		alert(1);
+		break;
+	case 0:
+		alert(0);
+		break;
+	case -5:
+		alert(-1);
+		break;
+}
+// if (value > 0) {
+// 	alert(1);
+// } else if (value < 0) {
+// 	alert(-1);
+// } else {
+// 	alert(0);
+// }
